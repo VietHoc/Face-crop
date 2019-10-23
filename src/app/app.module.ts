7aba7a956a56ee7a14a259456aaa0ffd885f68d5
viethoc <hocle@novahub.vn>
2019-10-23 17:35:07 +0700

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { AppComponent } from './app.component';
 import { AvatarComponent } from './avatar/avatar.component';
 import { ImageCropperModule } from 'ngx-image-cropper';
 import {WebcamModule} from 'ngx-webcam';
+import {NgOpenCVModule, OpenCVOptions} from 'ng-open-cv';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatGridListModule, MatSnackBarModule} from '@angular/material';
+
+const openCVConfig: OpenCVOptions = {
+  scriptUrl: `assets/opencv/opencv.js`,
+  wasmBinaryFile: 'wasm/opencv_js.wasm',
+  usingWasm: true
+};
+
 
 @NgModule({
   declarations: [
@@ -16,9 +26,17 @@ import {WebcamModule} from 'ngx-webcam';
     BrowserModule,
     AppRoutingModule,
     ImageCropperModule,
-    WebcamModule
+    WebcamModule,
+    NgOpenCVModule.forRoot(openCVConfig),
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSnackBarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+  ]
 })
 export class AppModule { }
--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ImageCroppedEvent, CropperPosition, ImageCropperComponent } from 'ngx-image-cropper';
-import { WebcamInitError, WebcamImage, WebcamUtil } from 'ngx-webcam';
-import { Subject, Observable } from 'rxjs';
-import { INIT_IMAGE_BASE_64 } from '../constant';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ImageCroppedEvent, CropperPosition, ImageCropperComponent} from 'ngx-image-cropper';
+import {WebcamInitError, WebcamImage, WebcamUtil} from 'ngx-webcam';
+import {Subject, Observable, BehaviorSubject, forkJoin} from 'rxjs';
+import {INIT_IMAGE_BASE_64} from '../constant';
 import * as smartcrop from 'smartcrop';
+import {NgOpenCVService, OpenCVLoadResult} from 'ng-open-cv';
+import {filter, switchMap, tap} from 'rxjs/operators';
+import {MatSnackBar} from '@angular/material';
+
 
 @Component({
   selector: 'app-avatar',
@@ -11,17 +15,24 @@ import * as smartcrop from 'smartcrop';
   styleUrls: ['./avatar.component.scss']
 })
 export class AvatarComponent implements OnInit {
-  // @ViewChild(ImageCropperComponent, {static: false}) imageCropper: ImageCropperComponent;s
+  imageChangedEvent: any = '';
   croppedImage: any = '';
   imageBase64 = INIT_IMAGE_BASE_64;
   img: any;
+  currentFace = 0;
   cropper: CropperPosition = {
     x1: 0,
     y1: 0,
     x2: 0,
     y2: 0
   };
-
+  options = {
+    width: 400,
+    height: 600,
+    minScale: 1,
+    boost: null,
+    debug: true
+  };
   public currentCropper: CropperPosition;
 
   // toggle webcam on/off
@@ -43,11 +54,119 @@ export class AvatarComponent implements OnInit {
   // switch to next / previous / specific webcam; true/false: forward/backwards, string: deviceId
   private nextWebcam: Subject<boolean | string> = new Subject<boolean | string>();
 
+
+  private classifiersLoaded = new BehaviorSubject<boolean>(false);
+  classifiersLoaded$ = this.classifiersLoaded.asObservable();
+
+  // HTML Element references
+  @ViewChild('canvasInput', {static: false}) canvasInput: ElementRef;
+  @ViewChild('canvasOutput', {static: false}) canvasOutput: ElementRef;
+
+
+  constructor(
+    private ngOpenCVService: NgOpenCVService,
+    private snackBar: MatSnackBar
+  ) {
+  }
+
   public ngOnInit(): void {
     WebcamUtil.getAvailableVideoInputs()
       .then((mediaDevices: MediaDeviceInfo[]) => {
         this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
       });
+
+    // Always subscribe to the NgOpenCVService isReady$ observer before using a CV related function to ensure that the OpenCV has been
+    // successfully loaded
+    this.ngOpenCVService.isReady$
+      .pipe(
+        // The OpenCV library has been successfully loaded if result.ready === true
+        filter((result: OpenCVLoadResult) => result.ready),
+        switchMap(() => {
+          // Load the face and eye classifiers files
+          return this.loadClassifiers();
+        })
+      )
+      .subscribe(() => {
+        // The classifiers have been succesfully loaded
+        this.classifiersLoaded.next(true);
+      });
+  }
+
+  loadClassifiers(): Observable<any> {
+    return forkJoin(
+      this.ngOpenCVService.createFileFromUrl(
+        'haarcascade_frontalface_default.xml',
+        `assets/opencv/data/haarcascades/haarcascade_frontalface_default.xml`
+      ),
+      this.ngOpenCVService.createFileFromUrl(
+        'haarcascade_eye.xml',
+        `assets/opencv/data/haarcascades/haarcascade_eye.xml`
+      )
+    );
+  }
+
+  detectFace() {
+    // before detecting the face we need to make sure that
+    // 1. OpenCV is loaded
+    // 2. The classifiers have been loaded
+    this.ngOpenCVService.isReady$
+      .pipe(
+        filter((result: OpenCVLoadResult) => result.ready),
+        switchMap(() => {
+          return this.classifiersLoaded$;
+        }),
+        tap(() => {
+          this.clearOutputCanvas();
+          this.findFaceAndEyes();
+        })
+      )
+      .subscribe(() => {
+        console.log('Face detected');
+      });
+  }
+
+  findFaceAndEyes() {
+    // Example code from OpenCV.js to perform face and eyes detection
+    // Slight adapted for Angular
+    const src = cv.imread(this.canvasInput.nativeElement.id);
+    const gray = new cv.Mat();
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+    const faces = new cv.RectVector();
+    const eyes = new cv.RectVector();
+    const faceCascade = new cv.CascadeClassifier();
+    const eyeCascade = new cv.CascadeClassifier();
+    // load pre-trained classifiers, they should be in memory now
+    faceCascade.load('haarcascade_frontalface_default.xml');
+    eyeCascade.load('haarcascade_eye.xml');
+    // detect faces
+    const msize = new cv.Size(0, 0);
+    faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+    console.log(faces);
+
+    for (let i = 0; i < faces.size(); ++i) {
+      const face = faces.get(i);
+      console.log(face);
+
+      const roiGray = gray.roi(faces.get(i));
+      const roiSrc = src.roi(faces.get(i));
+      const point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
+      const point2 = new cv.Point(faces.get(i).x + faces.get(i).width, faces.get(i).y + faces.get(i).height);
+      cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
+      // detect eyes in face ROI
+      eyeCascade.detectMultiScale(roiGray, eyes);
+      for (let j = 0; j < eyes.size(); ++j) {
+        const point3 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
+        const point4 = new cv.Point(eyes.get(j).x + eyes.get(j).width, eyes.get(j).y + eyes.get(j).height);
+        cv.rectangle(roiSrc, point3, point4, [0, 0, 255, 255]);
+      }
+      roiGray.delete();
+      roiSrc.delete();
+    }
+  }
+
+  clearOutputCanvas() {
+    const context = this.canvasOutput.nativeElement.getContext('2d');
+    context.clearRect(0, 0, this.canvasOutput.nativeElement.width, this.canvasOutput.nativeElement.height);
   }
 
   public triggerSnapshot(): void {
@@ -70,33 +189,55 @@ export class AvatarComponent implements OnInit {
   }
 
   public handleImage(webcamImage: WebcamImage): void {
-    console.info('received webcam image', webcamImage);
+    console.log('received webcam image', webcamImage);
     this.webcamImage = webcamImage;
     this.imageBase64 = webcamImage.imageAsDataUrl;
 
     this.img = new Image();
     this.img.crossOrigin = 'Anonymous';
     this.img.src = webcamImage.imageAsDataUrl;
-    this.img.onload = ( _ => {
-      this.run();
-    });
-  }
+    this.img.onload = (_ => {
+      const src = cv.imread(this.img);
+      const gray = new cv.Mat();
+      cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+      const faces = new cv.RectVector();
+      const faceCascade = new cv.CascadeClassifier();
+      // load pre-trained classifiers
+      faceCascade.load('haarcascade_frontalface_default.xml');
+      // detect faces
+      const msize = new cv.Size(0, 0);
+      faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+      console.log('faces:', faces.size());
 
-  run() {
-    const options = {
-      width: 400,
-      height: 600,
-      minScale: 1,
-      debug: true
-    };
-    this.analyze(options);
+      if (faces.size() > 0) {
+        this.currentFace = faces.size();
+        const boost = [];
+        for (let i = 0; i < faces.size(); ++i) {
+          const face = faces.get(i);
+          boost.push({
+            x: face.x,
+            y: face.y,
+            width: face.width,
+            height: face.height,
+            weight: 1.0
+          });
+        }
+        this.options.boost = boost;
+        setTimeout(_ => {
+          this.analyze(this.options);
+        });
+      } else {
+        this.currentFace = 0;
+        this.snackBar.open('Show your face in carema!', '', {
+          duration: 2000,
+        });
+      };
+    });
   }
 
   analyze(options) {
     smartcrop.crop(this.img, options).then(result => {
       console.log('smart crop:', result.topCrop);
-      // console.log(this.imageCropper);
-      // console.log(this.imageCropper.cropper);
       this.currentCropper = {
         x1: result.topCrop.x,
         y1: result.topCrop.y,
@@ -104,6 +245,10 @@ export class AvatarComponent implements OnInit {
         y2: result.topCrop.height + result.topCrop.y
       };
     });
+  }
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
   }
 
   public cameraWasSwitched(deviceId: string): void {
@@ -119,7 +264,6 @@ export class AvatarComponent implements OnInit {
     return this.nextWebcam.asObservable();
   }
 
-
   imageCropped(event: ImageCroppedEvent) {
     this.croppedImage = event.base64;
   }
@@ -131,8 +275,10 @@ export class AvatarComponent implements OnInit {
       x2: 0,
       y2: 0
     };
+
     console.log('this.currentCropper:', this.currentCropper);
-    
-    this.cropper = this.currentCropper ? this.currentCropper : defaultCropper;
+    setTimeout(_ => {
+      this.cropper = this.currentCropper ? this.currentCropper : defaultCropper;
+    });
   }
 }
